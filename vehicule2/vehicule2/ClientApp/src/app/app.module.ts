17c9f1613d1dd23fc6d6967d1039ebf03ca7fdcf
alainb-creator <alainbendejac@gmail.com>
2019-11-29 09:12:32 +0100

    maj3

--- vehicule2/vehicule2/ClientApp/src/app/app.module.ts
+++ vehicule2/vehicule2/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { VehiculeComponent } from './car/vehicule.component';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { VehiculeComponent } from './car/vehicule.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-        FetchDataComponent,
        VehiculeComponent
   ],
   imports: [
@@ -25,9 +21,7 @@ import { VehiculeComponent } from './car/vehicule.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-        { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' },     
      { path: 'car', component: VehiculeComponent },
     ])
   ],
